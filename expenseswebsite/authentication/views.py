01c2dee53e2e03cc898696f7483fed94d6f63c55
Micheline <MDoughlin@hotmail.com>
2023-11-11 21:49:23 -0500

    removed env

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -4,12 +4,49 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email
+from django.contrib import messages
+from django.core.mail import EmailMessage
 
 # Create your views here
 
 class RegistrationView(View):
   def get(self, request):
       return render(request, 'authentication/register.html')
+
+  def post(self, request):
+    #Get Data
+    #Validate
+    #Create A User Account
+    username=request.POST['username']
+    email=request.POST['email']
+    password=request.POST['password']
+
+    context={
+      'fieldValues':request.POST
+    }
+    if not User.objects.filter(username=username).exists():
+      if not User.objects.filter(email=email).exists():
+
+        if len(password) < 6:
+          messages.error(request, 'Password too short')
+          return render(request, 'authentication/register.html', context)
+
+        user = User.objects.create_user(username=username, email=email)
+        user.set_password(password)
+        user.is_active=False
+        user.save()
+        email_subject = 'Activate your account'
+        email_body = ''
+        email = EmailMessage(
+          email_subject,
+          email_body,
+          "noreply@expenseapp.com",
+          [email],
+)
+        messages.success(request, 'Account successfully created')
+        return render(request, 'authentication/register.html')
+
+    return render(request, 'authentication/register.html')
 
 class UsernameValidationView(View):
   def post(self, request):
